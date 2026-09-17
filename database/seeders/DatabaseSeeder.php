eb6f6ac6a97420153315ba305310103d5f0f7703
icfo-bookem <nationalambulancebd@gmail.com>
2026-09-17 11:40:22 +0600

    seeder problem sloved

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Modules\Frontend\Database\Seeders\FrontendDatabaseSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -21,5 +22,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $this->call(FrontendDatabaseSeeder::class);
     }
 }
